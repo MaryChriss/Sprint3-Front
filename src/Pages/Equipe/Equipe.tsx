864f076c6e20b9149fdabb88c25df53c63dc78de
Gabriela <gmgoncalves26@gmail.com>
2024-08-30 22:43:07 -0300

    Dando incio a page equipe

--- src/Pages/Equipe/Equipe.tsx
+++ src/Pages/Equipe/Equipe.tsx
@@ -1,9 +1,7 @@
-import { Layout } from "../../components/Layout/Layout";
+
 
 export default function Equipe() {
     return (
-        <Layout>
-        <h1>Equipe</h1>
-        </Layout>
+
     )
 }

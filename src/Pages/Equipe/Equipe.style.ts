a17ec5886e22a4d4c80040c9f8b1eeb8fd35f2b6
Gabriela <gmgoncalves26@gmail.com>
2024-08-30 22:44:26 -0300

    Codigo finalizado, estilização sendo feita

---
+++ src/Pages/Equipe/Equipe.style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledEquipe = styled.section`
+    background-color: #F1F8FF;
+`
+
+export const StyledInfos = styled.div`
+    display: flex;
+`
+
+export const StyledDetails2 = styled.div`
+        height: 25rem;
+        width: 17rem;
+
+        & img {
+            height: 38rem;
+            width: 20rem;
+            margin-left: 2rem;
+        }
+`
--- src/Pages/Equipe/Equipe.tsx
+++ src/Pages/Equipe/Equipe.tsx
@@ -1,7 +1,92 @@
-
+import { Layout } from "../../components/Layout/Layout";
+import { StyledDetails2, StyledEquipe, StyledInfos } from "./Equipe.style";
 
 export default function Equipe() {
     return (
+        <Layout>
+            <StyledEquipe>
 
+                <StyledInfos>
+
+                    <StyledDetails2>
+                        <img src="../img/TopicsPequen.png" alt="" />
+                    </StyledDetails2>
+
+                    <div>
+                        <h1>Nossa Equipe:</h1>
+
+                        <p>
+                            Conheça as pessoas por trás do nosso projeto. Nossa equipe está dedicada a oferecer <br />
+                            o melhor serviço e suporte para garantir que você tenha a melhor experiência possível.
+                        </p>
+                    </div>
+
+                </StyledInfos>
+
+                <div>
+
+                    <div>
+
+                        <img src="../img/mariFoto.jpg" alt="" className="imagem" />
+                        <p>Mariana Christina</p>
+
+                        <div>
+
+                            <div>
+                                <a href="https://github.com/MaryChriss">GitHub</a>
+                                <img src="../img/Github.png" alt="" />
+                            </div>
+
+                            <div>
+                                <a href="https://www.linkedin.com/in/mariana-fernandes-92690425a/">Linkedin</a>
+                                <img src="../img/Linkedin.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div>
+
+                        <img src="../img/henriqueFoto.png" alt="" className="imagem" />
+                        <p>Henrique Maciel</p>
+
+                        <div>
+
+                            <div>
+                                <a href="https://github.com/Maciel0123">GitHub</a>
+                                <img src="../img/Github.png" alt="" />
+                            </div>
+
+                            <div>
+                                <a href="https://www.linkedin.com/in/henrique-maciel-a70140312/">Linkedin</a>
+                                <img src="../img/Linkedin.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div>
+
+                        <img src="../img/gabiFoto.jpg" alt="" className="imagem" />
+                        <p>Gabriela Moguinho</p>
+
+                        <div>
+
+                            <div>
+                                <a href="https://github.com/gabrielamoguinho">GitHub</a>
+                                <img src="../img/Github.png" alt="" />
+                            </div>
+
+                            <div>
+                                <a href="https://www.linkedin.com/in/gabriela-moguinho-a18762265/">Linkedin</a>
+                                <img src="../img/Linkedin.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </StyledEquipe>
+
+        </Layout>
     )
 }

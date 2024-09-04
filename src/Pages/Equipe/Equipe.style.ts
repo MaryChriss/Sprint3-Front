7ab549ca1c05cf27f68c4a42b72525096a7da9b5
Gabriela <gmgoncalves26@gmail.com>
2024-09-04 20:19:41 -0300

    Criando component para o botao das redes

--- src/Pages/Equipe/Equipe.style.ts
+++ src/Pages/Equipe/Equipe.style.ts
@@ -65,22 +65,22 @@ export const StyledRedes = styled.div`
         margin-bottom: 2.5rem;
         gap: 2rem;
 `
-export const StyledMeios = styled.div`
-        background-color: white;
-        border-radius: 1.2rem;
-        border: lightgray 1px solid;
-        padding: 0.5rem;
-        height: 3rem;
-        width: 8rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 0.5rem;
+// export const StyledMeios = styled.div`
+//         background-color: white;
+//         border-radius: 1.2rem;
+//         border: lightgray 1px solid;
+//         padding: 0.5rem;
+//         height: 3rem;
+//         width: 8rem;
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+//         gap: 0.5rem;
         
 
-        & a {
-            font-size: 1.25rem;
-            font-weight: bold;
-            color: black;
-        }
-`
+//         & a {
+//             font-size: 1.25rem;
+//             font-weight: bold;
+//             color: black;
+//         }
+// `

9cd0813b476ef20727c16aae5d185b6ee50c510a
Gabriela <gmgoncalves26@gmail.com>
2024-09-08 15:53:46 -0300

    Responsividade (tablet : home) finalizada

--- src/Pages/Home/Home.style.ts
+++ src/Pages/Home/Home.style.ts
@@ -12,6 +12,10 @@ export const StyledDescription = styled.div`
         display: flex;
         flex-direction: column;
     }
+
+    @media (min-width: 600px) and (max-width:999px){
+        display: flex;
+    }
 `
 
 export const StyledText = styled.div`
@@ -38,13 +42,31 @@ export const StyledText = styled.div`
         margin-top: 5rem;
         margin-right: 20px;
 
-        & h1 {
+        h1 {
             font-size: 20px;
             margin-bottom: 10px;
             margin-bottom: 0;
         }
 
-        & p {
+        p {
+            margin-top: 0.5rem;
+            font-size: 15px;
+            margin-top: 1rem;
+        }
+    }
+
+    @media (min-width: 600px) and (max-width:999px){
+        display: flex;
+        flex-direction: column;
+        margin-left: 2rem;
+        margin-top: 5rem;
+        
+        h1 {
+            font-size: 18px;
+            margin-bottom: 0;
+        }
+
+        p {
             margin-top: 0.5rem;
             font-size: 15px;
             margin-top: 1rem;
@@ -79,6 +101,24 @@ export const StyledImgPrinc = styled.div`
             height: 6rem;
         }
     }
+
+    @media (min-width: 600px) and (max-width:999px){
+
+        img.fundo {
+            margin-top: 2rem;
+            margin-left: 0rem;
+            margin-right: 4rem;
+            height: 17rem;
+            width: 17rem;
+        }
+
+        img.decoration1 {
+            position: absolute;
+            margin-left: 14rem;
+            width: 6rem;
+            height: 6rem;
+        }
+    }
 `
 
 export const StyledSub = styled.div`
@@ -96,6 +136,11 @@ export const StyledSub = styled.div`
         margin-left: 2rem;
         margin-bottom: 2rem;
     }}
+
+    @media (min-width: 600px) and (max-width:999px){
+        margin-top: 8rem;
+        margin-left: 4rem;
+    }
 `
 
 export const StyledStepByStep = styled.article`
@@ -122,6 +167,17 @@ export const StyledStepByStep = styled.article`
         
         
     }
+
+    @media (min-width: 600px) and (max-width:999px){
+        min-height: 10rem;
+        margin: 5rem;
+        margin-top: 4rem;
+        margin-bottom: 4rem;
+        border-radius: 6rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 `
 
 export const StyledStepsContainer = styled.div`
@@ -137,6 +193,15 @@ export const StyledStepsContainer = styled.div`
         justify-content: center;
         align-items: center;
         margin-left: 0rem;
+    }
+
+    @media (min-width: 600px) and (max-width:999px){
+        display: flex;
+        flex-direction: column;
+        margin: 1rem;
+        min-width: 300px;
+        justify-content: center;
+        align-items: center;
     }
 `
 
@@ -157,6 +222,12 @@ export const StyledFirstStep = styled.div`
             p {
             font-size: 0.9rem;
             }
+        }
+
+        @media (min-width: 600px) and (max-width:999px){
+            display: flex;
+            flex-direction: row;
+            gap: 3rem;
         }
 `
 
@@ -190,6 +261,29 @@ export const StyledSteps = styled.div`
             margin-top: 1rem;
         }
     }
+
+    @media (min-width: 600px) and (max-width:999px){
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        border-radius: 10px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        p {
+            font-size: 14px;
+            width: 14rem;
+            height: 6rem;
+            margin-top: 1rem;
+        }
+
+        h3 {
+            margin-top: 1rem;
+            font-size: 16px;
+        }
+    }
 `
 
 export const StyledSecondStep = styled.div`
@@ -210,6 +304,12 @@ export const StyledSecondStep = styled.div`
             font-size: 0.9rem;
         }
         }
+
+        @media (min-width: 600px) and (max-width:999px){
+            display: flex;
+            flex-direction: row;
+            gap: 3rem;
+        }
 `
 
 export const StyledThirdStep = styled.div`
@@ -229,5 +329,15 @@ export const StyledThirdStep = styled.div`
             font-size: 1rem;
     }
 
+    }
+
+    @media (min-width: 600px) and (max-width:999px){
+        display: flex;
+        flex-direction: row;
+        margin-top: 0rem;
+
+        h3{
+            margin-bottom: 1rem;
+        }
     }
 `
